fe96cecf51758d5bc2fa0e3115e01b84d22b29b0
Luke Pratt <lukepratt@Lukes-MacBook-Air.local>
2023-09-15 20:23:30 -0800

    signup page works for me

--- FitnessTracker/myapp/forms.py
+++ FitnessTracker/myapp/forms.py
@@ -1,8 +1,54 @@
+from typing import Any
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs.update({
+            'required':'',
+            'name':'username',
+            'id':'username',
+            'type':'text',
+            'class':'form-input',
+            'placeholder':'Username',
+            'maxlength':'16',
+            'minlength':'4'
+
+        })
+        self.fields["email"].widget.attrs.update({
+            'required':'',
+            'name':'email',
+            'id':'email',
+            'type':'text',
+            'class':'form-input',
+            'placeholder':'Email',
+            'maxlength':'30',
+            'minlength':'6'
+
+        })
+        self.fields["password1"].widget.attrs.update({
+            'required':'',
+            'name':'password1',
+            'id':'password1',
+            'type':'password',
+            'class':'form-input',
+            'placeholder':'Password',
+            'maxlength':'16',
+            'minlength':'6'
+            
+        })
+        self.fields["password2"].widget.attrs.update({
+            'required':'',
+            'name':'password2',
+            'id':'password2',
+            'type':'password',
+            'class':'form-input',
+            'placeholder':'Confirm Password',
+            'maxlength':'16',
+            'minlength':'6'
+        })
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
--- FitnessTracker/myapp/views.py
+++ FitnessTracker/myapp/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import SignUpForm, UserCreationForm
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def index(request):
     return render(request, "index.html")
 
 def signup(request):
-    return render(request, "signup.html")
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data["username"]
+        email = form.cleaned_data["email"]
+        password = form.cleaned_data["password1"]
+        new_user = authenticate(username=username, email=email, password=password)
+        login(request, new_user)
+        return redirect("index")
+
+    form = SignUpForm()
+    context = {"form":form}
+    return render(request, "signup.html", context)
 
