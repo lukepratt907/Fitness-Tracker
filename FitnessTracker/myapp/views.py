47f88c8f2d6fa72f0e9790cb3fe2fccf85390c33
Hjriebe <hjriebe@alaska.edu>
2023-09-29 13:35:33 -0800

    Step back again to fix index view

--- FitnessTracker/myapp/views.py
+++ FitnessTracker/myapp/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def index(request):
-    return render(request, "myapp/index.html")
+    return render(request, "index.html")
 
 def signup(request):
     if request.method == 'POST':
