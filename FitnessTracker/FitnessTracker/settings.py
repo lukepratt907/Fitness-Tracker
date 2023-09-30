781c89dbbfdc447d4994f1e3e77e2384c6fc62d4
Hjriebe <hjriebe@alaska.edu>
2023-09-29 22:26:33 -0800

    add /

--- FitnessTracker/FitnessTracker/settings.py
+++ FitnessTracker/FitnessTracker/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/myapp"),
+    os.path.join(BASE_DIR, "static/myapp/"),
 ]
 
-STATIC_URL = '/static/myapp'
+STATIC_URL = '/static/myapp/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
