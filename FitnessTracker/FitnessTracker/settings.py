3e0de94b8c806d4356cc0ad915852028eafc41ba
Hjriebe <hjriebe@alaska.edu>
2023-09-29 22:30:36 -0800

    ok

--- FitnessTracker/FitnessTracker/settings.py
+++ FitnessTracker/FitnessTracker/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/myapp/"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 STATIC_URL = '/static/'
