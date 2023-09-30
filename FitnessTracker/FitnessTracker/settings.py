24385cf7ca707aadea53e6720a7b23ea57ccbf94
Hjriebe <hjriebe@alaska.edu>
2023-09-29 22:27:49 -0800

    fix again?

--- FitnessTracker/FitnessTracker/settings.py
+++ FitnessTracker/FitnessTracker/settings.py
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/myapp/"),
 ]
 
-STATIC_URL = '/static/myapp/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
