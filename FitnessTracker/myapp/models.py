529928b9f13766bf99541cb52ce4866cc55cef97
Hjriebe <hjriebe@alaska.edu>
2023-10-12 18:24:23 -0800

    hayden testing again

--- FitnessTracker/myapp/models.py
+++ FitnessTracker/myapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # hayden test commit
 # Hayden test again
+# Hayden
 
 # Create your models here.
 class Profile(models.Model):
