76e4514394ac9227ca2ef69d7985b7d76d9a92e4
Hjriebe <hjriebe@alaska.edu>
2023-10-18 17:31:10 -0800

    okay

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [ # baseurl/"urlbelow"
     path("", views.index, name="users-index"),#when we go to ""(base url/"") it calls views.home(prints hello world! atm) url called home
     path("signup/", views.signup, name="users-signup"),
-    
 ]
