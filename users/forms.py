448c5f008d5dd4724d1430ae23cfe388ebcc554e
Hjriebe <hjriebe@alaska.edu>
2023-10-21 22:06:29 -0800

    added password2 field

--- users/forms.py
+++ users/forms.py
@@ -22,7 +22,7 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password1', 'password2']
 
 """
 class SignUpForm(UserCreationForm):
