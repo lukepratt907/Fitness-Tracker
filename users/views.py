f84bd73f7cd5340abf9b3cd6b303dc4569f9cd91
Hjriebe <hjriebe@alaska.edu>
2023-10-22 14:07:37 -0800

    registration redirects to login now to ensure user data displayed in profile.html

--- users/views.py
+++ users/views.py
@@ -8,12 +8,12 @@ from django.contrib import messages
 
 def register_view(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+        form = UserRegisterForm(data=request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('users-profile')
+            return redirect('users-login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
