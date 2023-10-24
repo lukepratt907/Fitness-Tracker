2bf058afe679887e28a5b42bd80ed391f37f8137
keelersthetic2 <keelerjacobc@gmail.com>
2023-10-23 19:30:58 -0800

    Added some buttons to templates, added bootstrap links, made users-login to users-logout in registration.html

--- FitnessTracker/settings.py
+++ FitnessTracker/settings.py
@@ -119,11 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# Commented out below because it was giving a warning, and I am not sure if we even need it
+'''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+'''
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
