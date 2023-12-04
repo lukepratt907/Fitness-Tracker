9be9f731543f83c913ddc49d36da0982273bbd70
keelersthetic2 <keelerjacobc@gmail.com>
2023-12-04 13:14:55 -0900

    Exercise alphabetical ordering

--- workouts/models.py
+++ workouts/models.py
@@ -7,6 +7,9 @@ class Exercise(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        ordering = ('name',)
 
 class Workout(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
