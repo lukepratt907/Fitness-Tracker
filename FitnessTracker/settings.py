92005e1a7c93bfac9876ce608eaa8066ecd222c1
lukepratt <lukepratt@Lukes-MacBook-Air.local>
2023-12-03 15:41:50 -0900

    deployment bug fix

--- FitnessTracker/settings.py
+++ FitnessTracker/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-k0!6fx--(xits5*7y043_!u==ni1hl8x^suo-j8+guajd=gg=8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
